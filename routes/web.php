e891735a1b43dd4d6fc63ba6d73cb509195c4d55
soma <soma.ksg@gmail.com>
2025-09-05 20:44:28 +0900

    Redirect / to /equipment

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EquipmentController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('equipment.index');
 });
 
 Route::resource('equipment', EquipmentController::class);
