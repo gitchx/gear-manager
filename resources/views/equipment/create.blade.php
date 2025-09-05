4192ead378c0a216891310ede0151e6ed9fd49b8
soma <soma.ksg@gmail.com>
2025-09-05 21:36:14 +0900

    Imporove design of create view

--- resources/views/equipment/create.blade.php
+++ resources/views/equipment/create.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container mx-auto px-4 py-8">
+<div class="max-w-2xl mx-auto p-6 bg-base-100 rounded-lg shadow-md">
     <h1 class="text-2xl font-bold mb-6">機材をリストに追加</h1>
 
     @if ($errors->any())
